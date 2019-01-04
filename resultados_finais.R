5b097e70fb37ca51a346f26311f23972a9bd0430
Lucas Waldir Zorzetti <you@example.com>
2019-01-04 17:45:13 -0200

    iniciei a modelagem de taxas de captura, mas aparentemente não sobram muitos dados para trabalhar, dado que muitos não atacaram

--- resultados_finais.R
+++ resultados_finais.R
@@ -89,7 +89,119 @@ belo_model_log2 <- lm(log(taxacrescimento) ~ log(biomassant) + tratamento + bloc
 summary(belo_model)
 summary(belo_model_log)
 
-lrtest(belo_model, belo_model_log, belo_model_log1, belo_model_log2) #melhor o de log
+lrtest(belo_model, belo_model_log, belo_model_log1, belo_model_log2) #melhor o completo mesmo
+step(belo_model_log) #incluir tudo
 
 ###Notonectidae
+noto_model <- lm(taxacrescimento ~ biomassant + tratamento + bloco, data = notonectidae)
+noto_model_log <- lm(log(taxacrescimento) ~ log(biomassant) + tratamento + bloco, data = notonectidae)
 
+lrtest(noto_model, noto_model_log)
+step(noto_model_log)
+
+summary(noto_model_log)
+
+#Teste para NORMALIDADE (valores de p > 0,05 indicam dados normais)
+shapiro.test(rstudent(noto_model_log))   ##teste de shapiro wilk (normalidade)
+
+# Análise visual para homogeneidade dos resíduos (visualmente eles devem se distribuir igualmente #abaixo e acima da linha)
+plot(rstudent(noto_model_log) ~ fitted(noto_model_log), pch = 19)
+abline(h = 0, lty = 2)
+
+#Visualização gráfica lty é o tipo da linha 1: linha contínua; 2: linha descontínua
+plot(log(notonectidae$taxacrescimento)~log(notonectidae$biomassant))
+abline(noto_model_log,lty=2)
+
+plot(noto_model_log)
+
+notonectidae$taxacrescimento[16] <- NA
+notonectidae$taxacrescimento[1] <- NA
+
+anova(noto_model_log) #Só a biomassa se correlacionou, o resto não
+
+####Anisoptera
+aniso_model <- lm(taxacrescimento ~ biomassant + tratamento + bloco, data = anisoptera)
+aniso_model_log <- lm(log(taxacrescimento) ~ log(biomassant) + tratamento + bloco, data = anisoptera)
+
+lrtest(aniso_model, aniso_model_log)
+step(aniso_model_log)
+
+summary(aniso_model_log)
+
+#Teste para NORMALIDADE (valores de p > 0,05 indicam dados normais)
+shapiro.test(rstudent(aniso_model_log))   ##teste de shapiro wilk (normalidade)
+
+# Análise visual para homogeneidade dos resíduos (visualmente eles devem se distribuir igualmente #abaixo e acima da linha)
+plot(rstudent(aniso_model_log) ~ fitted(aniso_model_log), pch = 19)
+abline(h = 0, lty = 2)
+
+#Visualização gráfica lty é o tipo da linha 1: linha contínua; 2: linha descontínua
+plot(log(anisoptera$taxacrescimento)~log(anisoptera$biomassant))
+abline(aniso_model_log,lty=2)
+
+plot(aniso_model_log)
+
+anisoptera$taxacrescimento[4] <- NA
+anisoptera$taxacrescimento[16] <- NA
+anisoptera$taxacrescimento[21] <- NA
+
+anova(aniso_model_log) #o bloco está fazendo efeito!
+
+####Zygoptera
+zygo_model <- lm(taxacrescimento ~ biomassant + tratamento + bloco, data = zygoptera)
+zygo_model_log <- lm(log(taxacrescimento) ~ log(biomassant) + tratamento + fezmuda + bloco, data = zygoptera)
+
+lrtest(zygo_model, zygo_model_log)
+step(zygo_model_log)
+
+summary(zygo_model_log)
+
+#Teste para NORMALIDADE (valores de p > 0,05 indicam dados normais)
+shapiro.test(rstudent(zygo_model_log))   ##teste de shapiro wilk (normalidade)
+
+# Análise visual para homogeneidade dos resíduos (visualmente eles devem se distribuir igualmente #abaixo e acima da linha)
+plot(rstudent(zygo_model_log) ~ fitted(zygo_model_log), pch = 19)
+abline(h = 0, lty = 2)
+
+#Visualização gráfica lty é o tipo da linha 1: linha contínua; 2: linha descontínua
+plot(log(zygoptera$taxacrescimento)~log(zygoptera$biomassant))
+abline(zygo_model_log,lty=2)
+
+plot(zygo_model_log)
+
+zygoptera$taxacrescimento[24] <- NA
+zygoptera$taxacrescimento[32] <- NA
+
+anova(zygo_model_log)
+
+
+
+# modelos de taxa de captura ----------------------------------------------
+##Belostomatidae
+belo_model <- lm(taxacap1 ~ biomassant + tratamento + bloco + fezmuda, data = belostomatidae)
+belo_model_log <-  lm(log(taxacap1) ~ log(biomassant) + tratamento + bloco + fezmuda, data = belostomatidae)
+
+lrtest(belo_model, belo_model_log)
+step(belo_model_log) #incluir tudo
+
+#Sumário dos resultados do modelo
+summary(belo_model_log)
+
+#Teste para NORMALIDADE (valores de p > 0,05 indicam dados normais)
+shapiro.test(rstudent(belo_model_log))   ##teste de shapiro wilk (normalidade)
+
+# Análise visual para homogeneidade dos resíduos (visualmente eles devem se distribuir igualmente #abaixo e acima da linha)
+plot(rstudent(belo_model_log) ~ fitted(belo_model_log), pch = 19)
+abline(h = 0, lty = 2)
+
+#Visualização gráfica lty é o tipo da linha 1: linha contínua; 2: linha descontínua
+plot(log(belostomatidae$taxacap1)~log(belostomatidae$biomassant))
+abline(belo_model_log,lty=2)
+
+plot(belo_model_log)
+
+belostomatidae$taxacap1[]
+
+anova(belo_model_log)
+
+###
